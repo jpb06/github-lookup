a15ec5165d1f9f4ac989a3a8ca84e4e4f3f7f2c3
jpb06 <jpb.06@outlook.com>
2020-05-24 18:39:38 +0200

    Setting up frontend

---
+++ front/src/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
+import configureStore from "./redux/store/configure.store";
+import { Provider as ReduxProvider } from "react-redux";
+
+const store = configureStore();
+
+ReactDOM.render(
+  //<React.StrictMode>
+  <>
+    <ReduxProvider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </ReduxProvider>
+  </>,
+  //</React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
