4998d18675c01f656c66557340c390335904b7b8
jpb06 <jpb.06@outlook.com>
2020-05-26 18:03:59 +0200

    We must ensure axios is properly configured when loading a private route

--- front/src/components/logged-in/LoggedInContainer.tsx
+++ front/src/components/logged-in/LoggedInContainer.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
-import Topbar from "./generic/top-menu/TopMenu";
+import TopMenu from "./generic/top-menu/TopMenu";
 import styles from "./LoggedInContainer.styles";
 import Config from "../../logic/config";
 import { useHistory } from "react-router-dom";
+import LookupApi from "../../logic/api/setup/lookup.api";
+import { useReduxDispatch, useReduxSelector } from "../../hooks/redux.hooks";
+import GloballyBusy from "../generic/globally-busy/GloballyBusy";
+import { apiConfigured, getUserSaga } from "../../redux/actions";
 
 interface LoggedInContainerProps {
   Component: React.ElementType;
@@ -14,16 +18,25 @@ const LoggedInContainer: React.FC<LoggedInContainerProps> = ({
 }) => {
   const classes = styles();
   const history = useHistory();
+  const dispatch = useReduxDispatch();
+  const user = useReduxSelector((state) => state.user);
+  const isReady = useReduxSelector((state) => state.isApiConfigured);
 
   useEffect(() => {
     if (Config.token.length === 0) {
       history.push("/");
+    } else {
+      LookupApi.setup(history);
+      dispatch(apiConfigured(true));
+      dispatch(getUserSaga());
     }
-  }, [history]);
+  }, [history, dispatch]);
+
+  if (!isReady || !user) return <GloballyBusy text="Loading ... " />;
 
   return (
     <>
-      <Topbar />
+      <TopMenu user={user} />
       <div className={classes.content}>
         <Component {...rest} />
       </div>
