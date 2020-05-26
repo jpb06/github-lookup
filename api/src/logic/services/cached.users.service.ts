5656a397fa15dc97c7056fee28ee27dec178a5cd
jpb06 <jpb.06@outlook.com>
2020-05-26 17:46:17 +0200

    Moving service files to their own folder

--- api/src/logic/services/cached.users.service.ts
+++ api/src/logic/services/cached.users.service.ts
@@ -1,18 +1,18 @@
-import { LoggedUser } from "../types/logged.user.type";
+import { LoggedUser } from "../../types/logged.user.type";
 
 interface Dictionary<T> {
   [Key: string]: T;
 }
 
-export abstract class CacheService {
+export abstract class CachedUsersService {
   private static cachedUsers: Dictionary<LoggedUser> = {};
 
-  public static GetUserBy(id: number): LoggedUser | undefined {
+  public static GetBy(id: number): LoggedUser | undefined {
     let user = this.cachedUsers[id];
     return user;
   }
 
-  public static SetUser(user: LoggedUser): void {
+  public static Set(user: LoggedUser): void {
     this.cachedUsers[user.id] = user;
   }
 }
