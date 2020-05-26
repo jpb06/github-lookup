60d9ab1d5957883b265035a81c46acda7be73808
jpb06 <jpb.06@outlook.com>
2020-05-26 18:01:51 +0200

    Adding an helper to generate an action with payload

--- front/src/types/redux/action.with.payload.ts
+++ front/src/types/redux/action.with.payload.ts
@@ -1,6 +1,11 @@
 import { Action } from "redux";
-import { ActionTypes } from "./action.types";
+import { ActionType } from "./action.types";
 
-export interface ActionWithPayload<TPayload> extends Action<ActionTypes> {
+export interface ActionWithPayload<TPayload> extends Action<ActionType> {
   payload: TPayload;
 }
+
+export const payloadAction = (type: ActionType, payload: any) => ({
+  type,
+  payload,
+});
